0f896ee9faee8c12e3d5e32e0ba9e5705c92ae64
Megh patel <M3ghpatel1305@gmail.com>
2026-03-03 13:49:28 -0500

    Done

--- Club.java
+++ Club.java
@@ -4,17 +4,16 @@
  * @author (your name) 
  * @version 7.0
  */
+import java.util.ArrayList;
 public class Club
 {
-    // Define any necessary fields here ...
-    
+    private ArrayList<Membership>members;
     /**
      * Constructor for objects of class Club
      */
     public Club()
     {
-        // Initialise any fields here ...
-        
+        members = new ArrayList<>();
     }
 
     /**
@@ -23,6 +22,7 @@ public class Club
      */
     public void join(Membership member)
     {
+        members.add(member);
     }
 
     /**
@@ -31,6 +31,6 @@ public class Club
      */
     public int numberOfMembers()
     {
-        return 0;
+        return members.size();
     }
 }
